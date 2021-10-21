696dc5ea5621ff0fa43a0b1957787a43d12293a4
kakabisht <kakabisht07@gmail.com>
2021-10-21 23:59:53 +0530

    aplha version

--- App.js
+++ App.js
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Text, StyleSheet, View, ScrollView, Linking } from "react-native";
+import {
+  Card,
+  Provider,
+  Button,
+  Appbar,
+  Title,
+  Paragraph,
+} from "react-native-paper";
 
-export default function App() {
+const MyComponent = () => {
+  const _goBack = () => alert("Went Back");
+
+  const _handleMore = () => alert("Shown more");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={_goBack} />
+        <Appbar.Content
+          title="Our Application"
+          subtitle="By Aayushi, Bisht and Kavya"
+        />
+        <Appbar.Action icon="dots-vertical" onPress={_handleMore} />
+      </Appbar.Header>
+      <ScrollView>
+        <Card style={styles.cardbox}>
+          <Card.Cover
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/68463809?v=4",
+            }}
+          />
+          <Card.Actions>
+            <Card.Title title="Aayushi Verma" subtitle="Front End Developer" />
+          </Card.Actions>
+        </Card>
+        <Card style={styles.cardbox}>
+          <Card.Cover
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/41201308?v=4",
+            }}
+          />
+          <Card.Actions>
+            <Card.Title
+              title="Hridyesh Singh Bisht"
+              subtitle="Full Stack Developer"
+            />
+          </Card.Actions>
+        </Card>
+        <Card style={styles.cardbox}>
+          <Card.Cover
+            source={{
+              uri: "https://avatars.githubusercontent.com/u/59604782?v=4",
+            }}
+          />
+          <Card.Actions>
+            <Card.Title title="Kavya Suthar" subtitle="Data Analysis" />
+          </Card.Actions>
+        </Card>
+        <Button
+          icon="android"
+          mode="outlined"
+          onPress={() => Linking.openURL("https://kakabisht.github.io/")}
+          style={styles.btn}
+        >
+          Press me
+        </Button>
+      </ScrollView>
+    </Provider>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    width: 345,
+  },
+  cardbox: {
+    margin: 10,
+    backgroundColor: "#ebd4ff",
+  },
+  btn: {
+    marginLeft: 50,
+    marginRight: 50,
+  },
+  paragraph: {
+    alignContent: "center",
+    marginLeft: 10,
   },
 });
+export default MyComponent;
---
+++ readme.md
@@ -0,0 +1,2 @@
+Hello, this is our repo for our Mobile application presentation.
+In this React native application, we demonstrate a simple react native application using cards.
